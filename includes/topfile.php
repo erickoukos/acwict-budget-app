57e7104963ff9c0aa42b9779bac3c3c66d2571bc
erickoukos <erickoukos@gmail.com>
2022-07-30 09:58:22 +0300

    Updated topfile.php

--- includes/topfile.php
+++ includes/topfile.php
@@ -4,11 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<<<<<<< HEAD
     <link rel="stylesheet" href="../css/main.css">
-=======
-    <link rel="stylesheet" href="./css/style.css">
->>>>>>> 620a5c2dad02a876f6a93fb23819d587d9e7530c
     <title>Budget App</title>
     <!-- jQuery + Bootstrap JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
