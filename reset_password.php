8931f7113dfdb232265278b89bb6d4eb6e103c5f
erickoukos <erickoukos@gmail.com>
2022-08-16 15:24:28 +0300

    Modifies reset_password file

--- reset_password.php
+++ reset_password.php
@@ -22,11 +22,11 @@
                             <input type="hidden" name="email" value="<?php echo $email; ?>">
                             <input type="hidden" name="reset_link_token" value="<?php echo $token; ?>">
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Password</label>
+                                <label for="password">Password</label>
                                 <input type="password" name='password' class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Confirm Password</label>
+                                <label for="confirmPassword">Confirm Password</label>
                                 <input type="password" name='cpassword' class="form-control">
                             </div>
                             <input type="submit" name="new-password" class="btn btn-primary">
